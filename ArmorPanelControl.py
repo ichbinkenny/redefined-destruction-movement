5dd43536de800999f205372a5048435a784f2dd5
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-29 15:10:24 -0600

    Setup armor control to send info to parent process.

--- ArmorPanelControl.py
+++ ArmorPanelControl.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import sys
 
 GPIO.setmode(GPIO.BCM)
 
@@ -12,8 +13,7 @@ armor_panel_3 = GPIO.setup(armor_panel_3_pin, GPIO.IN)
 
 def readArmorStatus():
     armor_status = [GPIO.input(armor_panel_1_pin) == GPIO.HIGH, GPIO.input(armor_panel_2_pin) == GPIO.HIGH, GPIO.input(armor_panel_3_pin) == GPIO.HIGH]
-    ## TODO send armor status to parent input
-    print("A1 Connected: {}\nA2 Connected: {}\nA3 Connected: {}".format(armor_status[0], armor_status[1], armor_status[2]))
+    print("{}:{}:{}".format(armor_status[0], armor_status[1], armor_status[2]))
 
 if __name__ == "__main__":
     readArmorStatus()
