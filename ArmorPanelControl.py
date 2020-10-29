1f4d605f5d9ddaf623b0d5c343dbbb62315fc253
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-29 14:44:13 -0600

    Added ability to read armor panel presence.

---
+++ ArmorPanelControl.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+armor_panel_1_pin = 22
+armor_panel_2_pin = 23
+armor_panel_3_pin = 24
+
+armor_panel_1 = GPIO.setup(armor_panel_1_pin, GPIO.IN)
+armor_panel_2 = GPIO.setup(armor_panel_2_pin, GPIO.IN)
+armor_panel_3 = GPIO.setup(armor_panel_3_pin, GPIO.IN)
+
+def readArmorStatus():
+    armor_status = [GPIO.input(armor_panel_1_pin) == GPIO.HIGH, GPIO.input(armor_panel_2_pin) == GPIO.HIGH, GPIO.input(armor_panel_3_pin) == GPIO.HIGH]
+    ## TODO send armor status to parent input
+    print("A1 Connected: {}\nA2 Connected: {}\nA3 Connected: {}".format(armor_status[0], armor_status[1], armor_status[2]))
+
+if __name__ == "__main__":
+    readArmorStatus()
