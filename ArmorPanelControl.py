4e33e1d3196a4cfbec713e5145b6bd7716fe1ddc
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-29 16:12:16 -0600

    Converted true and false to 1 and 0 for easier processing.

--- ArmorPanelControl.py
+++ ArmorPanelControl.py
@@ -12,7 +12,7 @@ armor_panel_2 = GPIO.setup(armor_panel_2_pin, GPIO.IN)
 armor_panel_3 = GPIO.setup(armor_panel_3_pin, GPIO.IN)
 
 def readArmorStatus():
-    armor_status = [GPIO.input(armor_panel_1_pin) == GPIO.HIGH, GPIO.input(armor_panel_2_pin) == GPIO.HIGH, GPIO.input(armor_panel_3_pin) == GPIO.HIGH]
+    armor_status = [int(GPIO.input(armor_panel_1_pin) == GPIO.HIGH), int(GPIO.input(armor_panel_2_pin) == GPIO.HIGH), int(GPIO.input(armor_panel_3_pin) == GPIO.HIGH)]
     print("{}:{}:{}".format(armor_status[0], armor_status[1], armor_status[2]))
 
 if __name__ == "__main__":
