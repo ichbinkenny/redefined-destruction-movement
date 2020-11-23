4d9998425aad8427310446682902af199650a6f1
Kenneth Hunter <kennethdhunter@gmail.com>
2020-11-23 17:28:49 +0000

    Added pulldown resistor config for armor.

--- ArmorPanelControl.py
+++ ArmorPanelControl.py
@@ -7,9 +7,9 @@ armor_panel_1_pin = 22
 armor_panel_2_pin = 23
 armor_panel_3_pin = 24
 
-armor_panel_1 = GPIO.setup(armor_panel_1_pin, GPIO.IN)
-armor_panel_2 = GPIO.setup(armor_panel_2_pin, GPIO.IN)
-armor_panel_3 = GPIO.setup(armor_panel_3_pin, GPIO.IN)
+armor_panel_1 = GPIO.setup(armor_panel_1_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+armor_panel_2 = GPIO.setup(armor_panel_2_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+armor_panel_3 = GPIO.setup(armor_panel_3_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def readArmorStatus():
     armor_status = [GPIO.input(armor_panel_1_pin) == GPIO.HIGH, GPIO.input(armor_panel_2_pin) == GPIO.HIGH, GPIO.input(armor_panel_3_pin) == GPIO.HIGH]
